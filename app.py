cff401180735f8e130b3d111fad06170e2dba875
Pabu <pablovavrik@gmail.com>
2025-06-22 15:52:56 -0300

    Rotas UPDATE e DELETE para /Professor estão finalizadas

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def deleta_professor(id):
 
 #Rotas para o POST de /Professor
 @app.route('/Professor', methods = ['POST'])
-def create_professor():
+def criar_professor():
     novo_professor = request.get_json()
     
     if not novo_professor or 'id' not in novo_professor or 'nome' not in novo_professor: 
@@ -73,13 +73,13 @@ def create_professor():
 
 #Rotas para o GET de /Professor
 @app.route('/Professor', methods = ['GET'])
-def get_professor():
+def retornar_professor():
     professor = dados
     return jsonify(professor['Professor']), 200
 
 #POR ID
 @app.route('/Professor/<int:user_id>', methods = ['GET'])
-def get_professor_id(user_id):
+def retornar_professor_por_id(user_id):
     for professor in dados['Professor']:
         if professor['id'] == user_id:
             return jsonify(professor), 200
@@ -113,6 +113,44 @@ def limpar_campos_professor_por_id(user_id):
             return jsonify({'mensagem': f'Campos do Professor {user_id} foram resetados!', 
                     'Professor': professor}), 200
     return jsonify({'mensagem': f'Erro ao tentar resetar o professor de id {user_id}!'}), 404
+
+@app.route('/Professor/<int:user_id>', methods = ['PATCH'])
+def atualizar_professor_por_id(user_id):
+    dados_atualizados = request.get_json()
+    
+    if not dados_atualizados:
+        return jsonify({'mensagem' : 'Não há dados para atualizar!'}), 400
+    
+    for professor in dados['Professor']:
+        if professor['id'] == user_id:
+            for chave in dados_atualizados:
+                if chave in professor:
+                    professor[chave] = dados_atualizados[chave]
+            return jsonify({
+                'mensagem': 'Dados atualizados com sucesso!', 
+                'mensagem': professor
+                }), 200
+   
+    return jsonify({f'O Professor {user_id} não pode ser atualizado'}), 404 
+        
+
+#------------------------------------------------------------------
+
+#ROTA PARA DELETE de /Professor
+
+@app.route('/Professor/<int:user_id>', methods = ['DELETE'])
+def deletar_professor_por_id(user_id):
+
+    for i, professor in enumerate(dados['Professor']):
+        if professor['id'] == user_id:
+            dados['Professor'].pop(i)
+            return jsonify({'mensagem': f'O professor {user_id} foi deletado com sucesso!'}), 200
+    
+    return jsonify({'mensagem': f'Erro ao tentar deletar o professor {user_id}!'}), 404
+    
+    
+
+        
 
 if __name__ == '__main__':
     app.run(debug= True)
